73090f1b47c6af5f729eb76ca7d805357b7a6ef7
h9rythdscr-afk <h9rythdscr@privaterelay.appleid.com>
2026-06-29 12:58:24 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://vcoygnebhpnseddneyia.supabase.co";
+const SUPABASE_KEY = "sb_publishable_JnzH608H3DFyAJXWEjS0Hw_XNEi-BKY";
+
+const MAX_SIGNUPS = 10;
+
+// 报名截止时间：请按实际日期修改
+// 格式：2026-07-02T21:00:00+08:00
+const DEADLINE = "2026-07-02T21:00:00+08:00";
