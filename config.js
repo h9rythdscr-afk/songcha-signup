280f2bf1d51fb32f4f0a1c904d9525382ed568ad
h9rythdscr-afk <h9rythdscr@privaterelay.appleid.com>
2026-06-29 13:21:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://vcoygnebhpnseddneyia.supabase.co";
-const SUPABASE_KEY = "sb_publishable_JnzH608H3DFyAJXWEjS0Hw_XNEi-BKY";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZjb3lnbmViaHBuc2VkZG5leWlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2OTk4NjUsImV4cCI6MjA5ODI3NTg2NX0.1Et21JcM-ejPmb4irA1S81NCN25-0k5FDACIeLlowlI";
 
 const MAX_SIGNUPS = 10;
 
